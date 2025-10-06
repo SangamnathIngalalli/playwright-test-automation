620f0725e5b5d320bce53ba478bb8f12c77aeb3d
Sangamnath Ingalalli <sangamnath.professional@gmail.com>
2025-10-06 21:42:53 +0530

    removed old testcase

--- tests/ui/facebook-login.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('Facebook Login Tests', () => {
-  test('should login to Facebook', async ({ page }) => {
-    await page.goto('https://www.facebook.com/');
-    
-  });
-});
